c1ab9e1c428b63d32a7347cca125ad1e122dcff7
jannatunn <128748428+jannatunn@users.noreply.github.com>
2024-07-02 05:50:30 +0700

    9/29 middleware

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+
+
+export default function midlleware(request: NextRequest) {
+  const isLogin = false;
+  if(!isLogin){
+    return NextResponse.rewrite(new URL("/login", request.url))
+  }
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/about/:path*"]
+}
